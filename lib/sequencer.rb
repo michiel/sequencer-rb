a9e62b34c7ed47018f2df5cd4ba6a531653068c1
Michiel Kalkman <michiel@etryx.com>
2010-08-02 17:16:04 +0200

    Added some rdoc to sequencer

--- lib/sequencer.rb
+++ lib/sequencer.rb
@@ -1,21 +1,26 @@
+# A class for running arbitrary actions in sequence.
 class Sequencer
 
   def initialize(procs=[])
     @procs = procs
   end
 
+  # Add a function to the sequence
   def add(f)
     @procs.push(f)
   end
 
+  # Reverse the order of the sequence
   def reverse!
     @procs.reverse!
   end
 
+  # Start running
   def start
     self.next
   end
 
+  # Run the next function (if any)
   def next
     if @procs.size > 0
       @procs.shift.call(self)
